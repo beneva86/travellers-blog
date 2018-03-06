391f709773dc6cedd74852a3b15c8a93bd593be5
beneva86 <benikovszky.eva@gmail.com>
2018-03-06 11:52:02 +0100

    finish with the form validation

--- public/js/sendmessage.js
+++ public/js/sendmessage.js
@@ -1,60 +1,99 @@
-$("#commentForm").submit(function(event){
-    // cancels the form submission
-    event.preventDefault(); // ezzel megallitjuk a form szabalyos mukodeset, mert akkor frissulne az oldal
-    submitForm();
+// ------ check the email address and username in the database after giving the email address ------
+$('#input_email').focusout(function(){
+	let inputs = {
+		username: $('#username').val(),
+		email:$('#input_email').val()
+	};
+	$.post('/formvalidation', inputs, function(data) {
+		if (data.message === 'OK'){
+			$('.alert-danger').addClass('hidden');
+		} else {
+			$('.alert-danger').removeClass('hidden').text(`${data.message}`);
+		}
+	});
+});
+
+// ------ check the email address and username in the database after giving the username ------
+$('#username').focusout(function(){
+	let inputs = {
+		username: $('#username').val(),
+		email:$('#input_email').val()
+	};
+	$.post('/formvalidation', inputs, function(data) {
+		if (data.message === 'OK'){	
+			$('.alert-danger').addClass('hidden');
+		} else {
+			if($('#input_email').val() != ''){
+				$('.alert-danger').removeClass('hidden').text(`${data.message}`);
+			}
+		}
+	});
+});
+
+// ------ submit the form ------
+$('#commentForm').submit(function(event){
+	// cancels the form submission
+	event.preventDefault(); 
+
+	let username = $('#username').val();
+	let email = $('#input_email').val();
+	let message = $('#message').val();
+	if (username != '' && email != '' && message != '' && $('.alert-danger').hasClass('hidden')){
+		submitForm();
+	} 
 });
 
 function submitForm(){
-    let username = $("#username").val();
-    let email = $("#input_email").val();
-    let message = $("#message").val();
-    let topic = document.getElementById("title").innerHTML
- 
-    $.ajax({
-        type: "POST",
-        url: `/message_${topic}`,
-        data: "username=" + username + "&email=" + email + "&message=" + message + "&topic=" + topic,
-        success : function(response){
-            if (response.newMessage != undefined){
+	let username = $('#username').val();
+	let email = $('#input_email').val();
+	let message = $('#message').val();
+	let topic = document.getElementById('title').innerHTML;
+	let topicUrl = topic.replace(/\s/g, '');
+
+	$.ajax({
+		type: 'POST',
+		url: `/message_${topicUrl}`,
+		data: $('#commentForm').serialize() + '&topic=' + topic,
+		success : function(response){
+			if (response.newMessage != undefined){
 
 				function tableCreate(){
-					let parent = document.querySelector(".myCommentBox")
-					let commentForm = document.querySelector("#commentForm")
+					// let parent = document.querySelector('.myCommentBox');
+					let commentForm = document.querySelector('#commentForm');
 
-				    let tbl  = document.createElement('table');
-				    tbl.style.width  = '96%';
-    				tbl.style.border = '1px solid black';
+					let tbl  = document.createElement('table');
+					tbl.style.width  = '93%';
+					tbl.style.border = '1px solid black';
+					tbl.style.backgroundColor = '#ce787c17';
+					tbl.style.marginLeft = '6%';
+					tbl.style.marginTop = '-1%';
 
-			        let tr = tbl.insertRow();
-			        // let td = tr.insertCell();
-			        let tr2 = tbl.insertRow();
-			        // let td2 = tr.insertCell();
-			        let time = new Date().toLocaleTimeString().substring(0, 5)
-			        let date = new Date().toLocaleDateString()
+					let tr = tbl.insertRow();
+					let tr2 = tbl.insertRow();
+					let time = new Date().toLocaleTimeString().substring(0, 5);
+					let date = new Date().toLocaleDateString();
 
 					$(`<td(name='newMessage')><p style="padding-left: 2%; padding-top:1%">${username}</p></td>`).appendTo(tr);
 					$(`<td><p style='padding-left:4%'>${message}</p><span class='date sub-text'>${time} ${date}</span></td>`).appendTo(tr2);
 
-					parent.append(tbl)
-				    // commentForm.before(tbl)
-				    let scrollDiv = $(".myCommentBox");
+					// parent.append(tbl);
+					$('.noComments').addClass('hidden');
+					commentForm.before(tbl);
 
-				    // with animate 
-				    scrollDiv.animate({
-			            scrollTop: scrollDiv[0].scrollHeight - scrollDiv[0].clientHeight
-			          }, 1000);
-
-				    // without animate
-				    // scrollDiv.scrollTop(scrollDiv[0].scrollHeight - scrollDiv[0].clientHeight);
-				    
-
+					let scrollDiv = $('.myCommentBox');
+					// with animate 
+					scrollDiv.animate({
+						scrollTop: scrollDiv[0].scrollHeight - scrollDiv[0].clientHeight
+					}, 50);
+					// without animate
+					// scrollDiv.scrollTop(scrollDiv[0].scrollHeight - scrollDiv[0].clientHeight);
 				}
 				tableCreate();
-			    // Clear the form
-			    $('#username').val('');
-			    $('#input_email').val('');
-			    $('#message').val('');
+				// Clear the form
+				$('#username').val('');
+				$('#input_email').val('');
+				$('#message').val('');
 			}
 		}
-	})
-};
+	});
+}
--- src/app.js
+++ src/app.js
@@ -1,51 +1,34 @@
-const Sequelize = require('sequelize') 
-const express = require('express')
-const bodyParser = require('body-parser')
-// const session = require('express-session')
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// // BCRYPT
-// const bcrypt = require('bcrypt');
-// const saltRounds = 10;
+const Sequelize = require('sequelize'); 
+const express = require('express');
+const bodyParser = require('body-parser');
 
 // CONFIG dependencies
-const app = express()
+const app = express();
 
 // connect to the database
 const sequelize = new Sequelize('blog_application_bootstrap',process.env.POSTGRES_USER,null,{
   host: 'localhost',
   dialect: 'postgres',
   storage: './session.postgres' 
-})
+});
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
-app.set('views','views')
-app.set('view engine','pug')
+app.set('views','views');
+app.set('view engine','pug');
 
-app.use(bodyParser.urlencoded({extended: true}))
-
-// app.use(session({
-//   secret: "safe",         
-//   saveUnitialized: true,  
-//   resave: false,         
-//   store: new SequelizeStore({
-//     db: sequelize,  
-//     checkExpirationInterval: 15 * 60 * 1000, 
-//     expiration: 24 * 60 * 60 * 1000 
-//   })
-// }))
+app.use(bodyParser.urlencoded({extended: true}));
 
 //MODELS DEFINITION
 const User = sequelize.define('users', {
 	username: {
 		type: Sequelize.STRING,
-		unique: true
+		// unique: true
 	},
 	email: {
 		type: Sequelize.STRING,
 	}
-})
+});
 
 const Message = sequelize.define('messages', {
 	body: {
@@ -54,31 +37,17 @@ const Message = sequelize.define('messages', {
 	topic: {
 		type: Sequelize.TEXT
 	}
-})
-
-const Comment = sequelize.define('comments', {
-	body: {
-		type: Sequelize.TEXT 
-	}
-})
+});
 
 // TABLES RELATIONSHIP/ASSOCIATION 
-User.hasMany(Message)
-Message.belongsTo(User)	// userId in messages table
-Comment.belongsTo(Message) // messageId in comment table
-Comment.belongsTo(User) // userId in comment table
-User.hasMany(Comment)
-Message.hasMany(Comment)
+User.hasMany(Message);
+Message.belongsTo(User);	// userId in messages table
 
 //----------------ROUTES----------------
-
 //---------------ROUTE: HOME-----------------
 app.get('/', function(req, res){
-	// let user = req.session.user
-	// let message = req.query.message
-	// res.render('index', {user:user})
-	res.render('index')
-})	
+	res.render('index');
+})
 
 //-------------ROUTE: Costa Rica-------------
 app.get('/costarica', (req,res) => {
@@ -89,27 +58,46 @@ app.get('/costarica', (req,res) => {
 		},
 		include: [{
 			model: User
-		},
-		{
-			model: Comment
 		}]
 	}).then(messages => {
 		if(messages.length > 0) {
-			res.render('costarica', {messages:messages})
+			res.render('costarica', {messages:messages});
 		}
 		else {
-			res.render('costarica')
+			res.render('costarica');
 		}
 		
-	})
-})
+	});
+});
+
+//-------------ROUTE: Check the username and email address in the database -------------
+app.post('/formvalidation', (req,res) => {
+	let username = req.body.username;
+	let email = req.body.email;
+	console.log('---------------')
+	console.log(username)
+	console.log(email)
+
+	User.findOne({
+		where: {
+			username: username,
+		}
+	}).then( (user) => {
+		// if there is a match with the database
+		if(user!== null && user.email != email) {
+			res.send({message:'This username is already in use with an other email address'});
+		} else {
+			res.send({message: 'OK'});
+		}	
+	});
+});
 
 //-------------ROUTE: Create new message - Costa Rica-------------
 app.post('/message_costarica', (req,res) => {
-	const message = req.body.message
-	const topic = req.body.topic
-	const username = req.body.username
-	const email = req.body.email
+	const message = req.body.message;
+	const topic = req.body.topic;
+	const username = req.body.username;
+	const email = req.body.email;
 
 	User.create({
 		username: username,
@@ -120,9 +108,9 @@ app.post('/message_costarica', (req,res) => {
 			topic: topic,
 		})
 	}).then(newMessage => {
-		res.send({newMessage})
-	})
-})
+		res.send({newMessage});
+	});
+});
 
 //-------------ROUTE: Portugal-------------
 app.get('/portugal', (req,res) => {
@@ -132,26 +120,23 @@ app.get('/portugal', (req,res) => {
 		},
 		include: [{
 			model: User
-		},
-		{
-			model: Comment
 		}]
 	}).then(messages => {
 		if(messages.length > 0) {
-			res.render('portugal', {messages:messages})
+			res.render('portugal', {messages:messages});
 		}
 		else {
-			res.render('portugal')
+			res.render('portugal');
 		}		
-	})
-})
+	});
+});
 
 //-------------ROUTE: Create new message - Portugal-------------
 app.post('/message_portugal', (req,res) => {
-	const message = req.body.message
-	const topic = req.body.topic
-	const username = req.body.username
-	const email = req.body.email
+	const message = req.body.message;
+	const topic = req.body.topic;
+	const username = req.body.username;
+	const email = req.body.email;
 
 	User.create({
 		username: username,
@@ -162,38 +147,35 @@ app.post('/message_portugal', (req,res) => {
 			topic: topic,
 		})
 	}).then(newMessage => {
-		res.send({newMessage})
-	})
-})
+		res.send({newMessage});
+	});
+});
 
 //-------------ROUTE: Santorini-------------
 app.get('/santorini', (req,res) => {
 	Message.findAll({
 		where: {
-			topic: 'santorini'
+			topic: 'Santorini'
 		},
 		include: [{
 			model: User
-		},
-		{
-			model: Comment
 		}]
 	}).then(messages => {
 		if(messages.length > 0) {
-			res.render('santorini', {messages:messages})
+			res.render('santorini', {messages:messages});
 		}
 		else {
-			res.render('santorini')
+			res.render('santorini');
 		}		
-	})
-})
+	});
+});
 
 //-------------ROUTE: Create new message - Santorini-------------
 app.post('/message_santorini', (req,res) => {
-	const message = req.body.message
-	const topic = req.body.topic
-	const username = req.body.username
-	const email = req.body.email
+	const message = req.body.message;
+	const topic = req.body.topic;
+	const username = req.body.username;
+	const email = req.body.email;
 
 	User.create({
 		username: username,
@@ -202,31 +184,56 @@ app.post('/message_santorini', (req,res) => {
 		return user.createMessage({
 			body: message,
 			topic: topic,
-		})
+		});
 	}).then(newMessage => {
-		res.send({newMessage})
-	})
-})
+		res.send({newMessage});
+	});
+});
 
 //-------------ROUTE: Sri Lanka-------------
 app.get('/srilanka', (req,res) => {
-	// const user = req.session.user
-	// res.render('srilanka', {user:user})
-	res.render('srilanka')
-})
+	Message.findAll({
+		where: {
+			topic: 'Sri Lanka'
+		},
+		include: [{
+			model: User
+		}]
+	}).then(messages => {
+		if(messages.length > 0) {
+			res.render('srilanka', {messages:messages});
+		}
+		else {
+			res.render('srilanka');
+		}		
+	});
+});
 
-//-------------ROUTE: thailand-------------
-app.get('/thailand', (req,res) => {
-	// const user = req.session.user
-	// res.render('thailand', {user:user})
-	res.render('thailand')
-})
+//-------------ROUTE: Create new message - Sri Lanka-------------
+app.post('/message_srilanka', (req,res) => {
+	const message = req.body.message;
+	const topic = req.body.topic;
+	const username = req.body.username;
+	const email = req.body.email;
 
-sequelize.sync()
+	User.create({
+		username: username,
+		email: email
+	}).then(user => {
+		return user.createMessage({
+			body: message,
+			topic: topic,
+		});
+	}).then(newMessage => {
+		res.send({newMessage});
+	});
+});
+
+sequelize.sync();
 
 app.listen(3000, function(){
-  console.log("Traveller's blog app listening on port 3000")
-})
+	console.log('Traveller\'s blog app listening on port 3000');
+});
 
 
 
